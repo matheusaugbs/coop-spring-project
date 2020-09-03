31f2af46538b07757c094dc181496eb9c0669c18
Matheus Augusto <matheus_silva.barreto@hotmail.com.br>
2020-09-02 23:46:40 -0300

    Projeto criado

---
+++ src/main/java/com/matheusaugbs/coopspringproject/CoopspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.matheusaugbs.coopspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopspringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusaugbs/coopspringproject/CoopspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusaugbs.coopspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
